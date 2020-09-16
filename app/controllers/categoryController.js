69a79f5df6279a760b8ddfbca4d8d388c6b5c45f
Bryan Soh <bryansoh95@gmail.com>
2020-09-16 12:02:56 +0800

    [shizhan/ch82/category-crud] Made minor corrections to categoryController and categoryService.

--- app/controllers/categoryController.js
+++ app/controllers/categoryController.js
@@ -3,62 +3,59 @@ const { sendErrorResponse } = require('../common/error/errorHandler');
 const CategoryService = require('../services/categoryService');
 
 module.exports = {
-    createCategory: async(req, res) => {
-        try {
-            const categoryData = req.body;
-            let category;
-            await sequelize.transaction(async (transaction) => {
-                category = await CategoryService.createCategory(categoryData, transaction);
-            });
-            return res.status(200).send(category);
-        } catch (err) {
-            console.log(err)
-            sendErrorResponse(res, err);
-        }
-    },
-
-    retrieveCategory: async(req, res) => {
-        try {
-            const { id } = req.params;
-            let category = await CategoryService.retrieveCategory(id);
-            return res.status(200).send(category);
-        } catch (err) {
-            sendErrorResponse(res, err);
-        }
-    },
-
-    retrieveAllCategory: async(req, res) => {
-        try {
-            let categories = await CategoryService.retrieveAllCategory();
-            return res.status(200).send(categories);
-        } catch (err) {
-            sendErrorResponse(res, err);
-        }
-    },
-
-    updateCategory: async(req, res) => {
-        try {
-            const { id } = req.params;
-            const categoryData = req.body;
-            let category;
-            await sequelize.transaction(async (transaction) => {
-                category = await CategoryService.updateCategory(id, categoryData, transaction);
-            });
-            return res.status(200).send(category);
-        } catch (err) {
-            console.log(err);
-            sendErrorResponse(res, err);
-        }
-    },
-
-    deleteCategory: async(req, res) => {
-        try {
-            const { id } = req.params;
-            CategoryService.deleteCategory(id);
-            return res.status(200).send();
-        } catch (err) {
-            console.log("pass")
-            sendErrorResponse(res, err);
-        }
+  createCategory: async(req, res) => {
+    try {
+      const categoryData = req.body;
+      let category;
+      await sequelize.transaction(async (transaction) => {
+          category = await CategoryService.createCategory(categoryData, transaction);
+      });
+      return res.status(200).send(category);
+    } catch (err) {
+      sendErrorResponse(res, err);
     }
+  },
+
+  retrieveCategory: async(req, res) => {
+    try {
+      const { id } = req.params;
+      let category = await CategoryService.retrieveCategory(id);
+      return res.status(200).send(category);
+    } catch (err) {
+      sendErrorResponse(res, err);
+    }
+  },
+
+  retrieveAllCategory: async(req, res) => {
+    try {
+      let categories = await CategoryService.retrieveAllCategory();
+      return res.status(200).send(categories);
+    } catch (err) {
+      sendErrorResponse(res, err);
+    }
+  },
+
+  updateCategory: async(req, res) => {
+    try {
+      const { id } = req.params;
+      const categoryData = req.body;
+      let category;
+      await sequelize.transaction(async (transaction) => {
+        category = await CategoryService.updateCategory(id, categoryData, transaction);
+      });
+      return res.status(200).send(category);
+    } catch (err) {
+      sendErrorResponse(res, err);
+    }
+  },
+
+  deleteCategory: async(req, res) => {
+    try {
+      const { id } = req.params;
+      CategoryService.deleteCategory(id);
+      return res.status(200).send();
+    } catch (err) {
+      sendErrorResponse(res, err);
+    }
+  }
 }
--- app/services/categoryService.js
+++ app/services/categoryService.js
@@ -5,53 +5,52 @@ const CustomError = require('../common/error/customError');
 const Category = require('../models/Category');
 
 module.exports = {
-    createCategory: async(categoryData, transaction) => {
-        const { name, description } = categoryData;
-        Checker.ifEmptyThrowError(name, Constants.Error.CategoryNameRequired);
-        if(!Checker.isEmpty(await Category.findOne({ where : { name } }))) {
-            throw new CustomError(Constants.Error.CategoryNameExist);
-        }
-        const category = await Category.create(categoryData, { transaction })
-        return category;
-    },
-
-    retrieveCategory: async(id) => {
-        const category = await Category.findByPk(id);
-
-        Checker.ifEmptyThrowError(category, Constants.Error.CategoryNotFound);
-        return category;
-    },
-
-    retrieveAllCategory: async() => {
-        const categories = Category.findAll();
-        return categories;
-    },
-
-    updateCategory: async(id, categoryData, transaction) => {
-        Checker.ifEmptyThrowError(id, Constants.Error.IdRequired);
-        let category = await Category.findByPk(id);
-        Checker.ifEmptyThrowError(category, Constants.Error.CategoryNotFound);
-        const updateKeys = Object.keys(categoryData);
-        if(updateKeys.includes('name')) {
-            Checker.ifEmptyThrowError(Constants.Error.CategoryNameRequired);
-        }
-        category = Category.update(
-            categoryData,
-            { returning: true, transaction, 
-                where : {
-                id
-            }
-        });
-
-        return category;
-    },
-
-    deleteCategory: async(id) => {
-        Checker.ifEmptyThrowError(id, Constants.Error.IdRequired);
-        let category = await Category.findByPk(id);
-        console.log(category)
-        Checker.ifEmptyThrowError(category, Constants.Error.CategoryNotFound);
-        console.log("pass+++++")
-        Category.destroy({ where: { id } });
+  createCategory: async(categoryData, transaction) => {
+    const { name, description } = categoryData;
+    Checker.ifEmptyThrowError(name, Constants.Error.CategoryNameRequired);
+    if(!Checker.isEmpty(await Category.findOne({ where : { name } }))) {
+      throw new CustomError(Constants.Error.CategoryNameExist);
     }
+    const category = await Category.create(categoryData, { transaction })
+    return category;
+  },
+
+  retrieveCategory: async(id) => {
+    const category = await Category.findByPk(id);
+
+    Checker.ifEmptyThrowError(category, Constants.Error.CategoryNotFound);
+    return category;
+  },
+
+  retrieveAllCategory: async() => {
+    const categories = Category.findAll();
+    return categories;
+  },
+
+  updateCategory: async(id, categoryData, transaction) => {
+    Checker.ifEmptyThrowError(id, Constants.Error.IdRequired);
+    let category = await Category.findByPk(id);
+    Checker.ifEmptyThrowError(category, Constants.Error.CategoryNotFound);
+    const updateKeys = Object.keys(categoryData);
+    if(updateKeys.includes('name')) {
+        Checker.ifEmptyThrowError(Constants.Error.CategoryNameRequired);
+    }
+    category = Category.update(
+      categoryData,
+      { returning: true, transaction, 
+        where : {
+          id
+        }
+    });
+
+    return category;
+  },
+
+  deleteCategory: async(id) => {
+    // To check for associated products
+    Checker.ifEmptyThrowError(id, Constants.Error.IdRequired);
+    let category = await Category.findByPk(id);
+    Checker.ifEmptyThrowError(category, Constants.Error.CategoryNotFound);
+    Category.destroy({ where: { id } });
+  }
 }
