68be8a2e7be2d8fee92c8f625823bf4469960651
Bryan Soh <bryansoh95@gmail.com>
2020-09-20 18:16:03 +0800

    Modified dummyData.js.

--- dummyData.js
+++ dummyData.js
@@ -1,15 +1,17 @@
 const Category = require('./app/models/Category');
-const Kiosk = require('./app/models/Kiosk');
 const CustomerService = require('./app/services/customerService');
 const MerchantService = require('./app/services/merchantService');
 const StaffService = require('./app/services/staffService');
 
 const addDummyData = async () => {
-  await StaffService.createStaff({ firstName: 'Jefferson', lastName: 'Low', mobileNumber: '91234567', password: 'Password123!', email: 'jefflowzh@live.com', staffRoleEnum: 'Admin' });
-  await CustomerService.createCustomer({ firstName: 'Alice', lastName: 'Ang', mobileNumber: '91234567', password: 'Password123!', email: 'alice@email.com' });
-  await MerchantService.createMerchant({ name: 'Nike', mobileNumber: '91234567', password: 'Password123!', email: 'nike@email.com' })
+  await StaffService.createStaff({ firstName: 'Alice', lastName: 'Ang', mobileNumber: '91234567', password: 'Password123!', email: 'alice@email.com', staffRoleEnum: 'Admin' });
+  await CustomerService.createCustomer({ firstName: 'Ben', lastName: 'Bek', mobileNumber: '92345678', password: 'Password123!', email: 'ben@email.com' });
+  await MerchantService.createMerchant({ name: 'Nike', mobileNumber: '93456789', password: 'Password123!', email: 'nike@email.com' });
+  const customerId = (await CustomerService.retrieveAllCustomers())[0].id;
+  const merchantId = (await MerchantService.retrieveAllMerchants())[0].id;
+  await CustomerService.activateCustomer(customerId);
+  await MerchantService.approveMerchant(merchantId);
   await Category.create({ name: 'Fashion & Apparel', description: 'Sample Description' });
-  await Kiosk.create({ lat: 1.3521, long: 103.8198, description: 'Located at the West Wing' });
 };
 
 addDummyData();
