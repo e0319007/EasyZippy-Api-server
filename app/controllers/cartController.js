ef74efdd63de6ae30351178fe507071aaf15675c
shizhan <e0322717@u.nus.edu>
2020-09-28 11:02:10 +0800

    [shizhan/ch89/cart-and-line-item-crud] create cart service and controller, add set cart method when initiating customer

---
+++ app/controllers/cartController.js
@@ -0,0 +1,6 @@
+const sequelize = require('../common/database');
+const { sendErrorResponse } = require('../common/error/errorHandler');
+
+module.exports = {
+  
+}
---
+++ app/services/cartService.js
@@ -0,0 +1,7 @@
+const Checker = require('../common/checker');
+const Constants = require('../common/constants');
+const CustomError = require('../common/error/customError');
+
+module.exports = {
+  
+}
