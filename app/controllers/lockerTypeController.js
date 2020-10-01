feb7ae7d67312f2278cf62de479aa4312dc40a08
shizhan <e0322717@u.nus.edu>
2020-10-01 11:49:47 +0800

    [shizhan/ch127/lockertype-crud] add locker type controller crud

--- app/controllers/lockerTypeController.js
+++ app/controllers/lockerTypeController.js
@@ -1,47 +1,66 @@
 const sequelize = require('../common/database');
 const { sendErrorResponse } = require('../common/error/errorHandler');
 
+
+const LockerTypeService = require('../services/lockerTypeService');
 module.exports = {
   createLockerType: async(req, res) => {
     try {
-      
-      return res.status(200).send();
+      const lockerTypeData = req.body;
+      let lockerType;
+      await sequelize.transaction(async (transaction) => {
+        lockerType = await LockerTypeService.createLockerType(lockerTypeData, transaction);
+    });
+      return res.status(200).send(lockerType);
     } catch(err) {
+      console.log(err);
       sendErrorResponse(res, err);
     }
   },
 
   updateLockerType: async(req, res) => {
     try {
- 
-      return res.status(200).send();
+      const lockerTypeData = req.body;
+      let lockerType;
+      const { id } = req.params;
+      await sequelize.transaction(async (transaction) => {
+        lockerType = await LockerTypeService.updateLockerType(id, lockerTypeData, transaction);
+    });
+      return res.status(200).send(lockerType);
     } catch(err) {
+      console.log(err);
       sendErrorResponse(res, err);
     }
   },
 
   retrieveLockerType: async(req, res) => {
     try {
- 
-      return res.status(200).send();
+      const { id } = req.params;
+      let lockerType = await LockerTypeService.retrieveLockerType(id);
+      return res.status(200).send(lockerType);      
     } catch(err) {
+      console.log(err);
       sendErrorResponse(res, err);
     }
   },
 
   retrieveAllLockerTypes: async(req, res) => {
     try {
- 
-      return res.status(200).send();
+      let lockerTypes = await LockerTypeService.retrieveAllLockerType();
+      return res.status(200).send(lockerTypes);
     } catch(err) {
       sendErrorResponse(res, err);
     }
   },
 
-  disableLockerType: async(req, res) => {
+  toggleDisableLockerType: async(req, res) => {
     try {
- 
-      return res.status(200).send();
+      const { id } = req.params;
+      let lockerType;
+      await sequelize.transaction(async (transaction) => {
+        lockerType = await LockerTypeService.toggleDisableLockerType(id, transaction);
+      })
+      return res.status(200).send(lockerType);      
     } catch(err) {
       sendErrorResponse(res, err);
     }
@@ -49,7 +68,9 @@ module.exports = {
 
   deleteLockerType: async(req, res) => {
     try {
- 
+      let { id } = req.params;
+      await LockerTypeService.deleteLockerType(id);
+      console.log(id)      
       return res.status(200).send();
     } catch(err) {
       sendErrorResponse(res, err);
