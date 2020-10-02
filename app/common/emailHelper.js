68421362566e0d7a176a67ca92460f910474ab7c
shizhan <e0322717@u.nus.edu>
2020-10-02 15:47:00 +0800

    [shizhan/ch136/send-email-after-approving-merchant] add email merchant for account approval

--- app/common/emailHelper.js
+++ app/common/emailHelper.js
@@ -45,6 +45,41 @@ module.exports = {
     let token;
     token = await crypto.randomBytes(25).toString('hex').slice(0,50);
     return token;
+  },
+
+  sendEmailForMerchantApproval: async(email) => {
+    try{
+      
+      let testAccount = await nodemailer.createTestAccount();
+
+      // create reusable transporter object using the default SMTP transport
+      let transporter = nodemailer.createTransport({
+
+       service:'gmail',
+        auth: {
+          user: 'easyzippyhelp@gmail.com', // generated ethereal user
+          pass: 'Easyzippy123!', // generated ethereal password
+        },
+      });
+
+      // send mail with defined transport object
+      let info = await transporter.sendMail({
+        from: 'easyzippyhelp@gmail.com', // sender address
+        to: email, // list of receivers
+        subject: 'Easy Zippy Account Approval', // Subject line
+        text: 'You are receiving this because your merchant account has been approved \n' + 
+        'If you did not request this, please ignore this email and your password will remain unchanged.\n'
+      });
+
+      console.log('Message sent: %s', info.messageId);
+      // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+      // Preview only available when sending through an Ethereal account
+      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+      // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+    } catch (err) {
+      console.log(err)
+    }
   }
 
 }
