befead3628ed34a0a03bf0dbc23810efb0daac7f
shizhan <e0322717@u.nus.edu>
2020-10-02 11:34:01 +0800

    [shizhan/ch88/bookingpackagemodel-crud] create booking package model service and controller

---
+++ app/controllers/bookingPackageModelController.js
@@ -0,0 +1,8 @@
+const { sendErrorResponse } = require('../common/error/errorHandler');
+const sequelize = require('../common/database');
+
+const BookingPackageModelService = require('../services/bookingPackageModelService');
+
+module.exports = {
+
+}
