7eec513448b14637de6929f027d9420c7e62102a
shizhan <e0322717@u.nus.edu>
2020-10-04 15:11:04 +0800

    [shizhan/ch135/promotion-crud] add promotion controller methods

--- app/controllers/promotionController.js
+++ app/controllers/promotionController.js
@@ -1,10 +1,27 @@
 const { sendErrorResponse } = require('../common/error/errorHandler');
 const sequelize = require('../common/database');
+const PromotionService = require('../services/promotionService');
 
 module.exports = {
-  createPromotion: async(req, res) => {
+  createMerchantPromotion: async(req, res) => {
     try {
-      
+      const promoData = req.body;
+      await sequelize.transaction(async (transaction) => {
+        promotion = await PromotionService.createMerchantPromotion(promoData, transaction);
+      });
+      return res.status(200).send(promotion);
+    } catch (err) {
+      console.log(err)
+      sendErrorResponse(res, err);
+    }
+  },
+
+  createMallPromotion: async(req, res) => {
+    try {
+      const promoData = req.body;
+      await sequelize.transaction(async (transaction) => {
+        promotion = await PromotionService.createMallPromotion(promoData, transaction);
+      });
       return res.status(200).send(promotion);
     } catch (err) {
       console.log(err)
@@ -14,8 +31,44 @@ module.exports = {
 
   updatePromotion: async(req, res) => {
     try {
-      
+      const promoData = req.body;
+      let promotion;
+      await sequelize.transaction(async (transaction) => {
+        promotion = await PromotionService.updatePromotion(promoData, transaction);
+      });
       return res.status(200).send(promotion);
+    } catch (err) {
+      console.log(err)
+      sendErrorResponse(res, err);
+    }
+  },
+
+  retrieveMallPromotion: async(req, res) => {
+    try {
+      return res.status(200).send(await PromotionService.retrieveMallPromotion());
+    } catch (err) {
+      console.log(err)
+      sendErrorResponse(res, err);
+    }
+  },
+
+  retrieveMerchantPromotionByMerchantId: async(req, res) => {
+    try {
+      const { id } = req.params
+      return res.status(200).send(await PromotionService.retrieveMerchantPromotionByMerchantId(id));
+    } catch (err) {
+      console.log(err)
+      sendErrorResponse(res, err);
+    }
+  },
+
+  deletePromotion: async(req, res) => {
+    try {
+      const { id } = req.params
+      await sequelize.transaction(async (transaction) => {
+        await PromotionService.deletePromotion(id, transaction);
+      });
+      return res.status(200).send();
     } catch (err) {
       console.log(err)
       sendErrorResponse(res, err);
--- app/services/promotionService.js
+++ app/services/promotionService.js
@@ -1,36 +1,94 @@
 const Checker = require('../common/checker');
 const Constants = require('../common/constants');
 const CustomError = require('../common/error/customError');
+const Merchant = require('../models/Merchant');
 const Promotion = require('../models/Promotion');
+const Staff = require('../models/Staff');
 
 module.exports = {
   createMerchantPromotion: async(promotionData, transaction) => {
-    const { promoCode, startDate, endDate, promoCode, description, termsAndConditions, percentageDiscount, flatDiscount, usageLimit, image }
+    let { promoCode, startDate, endDate, description, termsAndConditions, percentageDiscount, flatDiscount, usageLimit, merchantId, promotionType} = promotionData;
+    Checker.ifEmptyThrowError(promoCode, 'Promo Code' + Constants.Error.XXXIsRequired);
+    Checker.ifEmptyThrowError(startDate, 'Start date' + Constants.Error.XXXIsRequired);
+    Checker.ifEmptyThrowError(endDate, 'End date' + Constants.Error.XXXIsRequired);
+    Checker.ifEmptyThrowError(usageLimit, 'End date' + Constants.Error.XXXIsRequired);
+    Checker.ifEmptyThrowError(merchantId, 'Merchant ID' + Constants.Error.XXXIsRequired);
+    Checker.ifEmptyThrowError(await Merchant.findByPk(merchantId), Constants.Error.MerchantNotFound);
+    if(startDate > endDate) {
+      throw new CustomError(Constants.Error.StartDateLaterThanEndDate);
+    }
+    promotionType = Constants.PromotionType.MerchantPromotion;
+    
+    if(Checker.isEmpty(percentageDiscount) && Checker.isEmpty(flatDiscount)) {
+      throw new CustomError('Percentage discount or flat discount ' + Constants.Error.XXXIsRequired);
+    }
+    let promotion = await Promotion.create(promotionData, { transaction });
+    return promotion;
   },
 
   createMallPromotion: async(promotionData, transaction) => {
+    let { promoCode, startDate, endDate, description, termsAndConditions, percentageDiscount, flatDiscount, usageLimit, staffId, promotionType} = promotionData;
+    Checker.ifEmptyThrowError(promoCode, 'Promo Code' + Constants.Error.XXXIsRequired);
+    Checker.ifEmptyThrowError(startDate, 'Start date' + Constants.Error.XXXIsRequired);
+    Checker.ifEmptyThrowError(endDate, 'End date' + Constants.Error.XXXIsRequired);
+    Checker.ifEmptyThrowError(usageLimit, 'Usage Limit' + Constants.Error.XXXIsRequired);
+    Checker.ifEmptyThrowError(staffId, 'Staff ID' + Constants.Error.XXXIsRequired);
+    Checker.ifEmptyThrowError(await Staff.findByPk(staffId), Constants.Error.StaffNotFound);
+    if(startDate > endDate) {
+      throw new CustomError(Constants.Error.StartDateLaterThanEndDate);
+    }
+    promotionType = Constants.PromotionType.MallPromotion;
     
+    if(Checker.isEmpty(percentageDiscount) && Checker.isEmpty(flatDiscount)) {
+      throw new CustomError('Percentage discount or flat discount ' + Constants.Error.XXXIsRequired);
+    }
+    let promotion = await Promotion.create(promotionData, { transaction });
+    return promotion;
   },
 
-  updatePromotion: async(promotionData, transaction) => {
-    
+  updatePromotion: async(id, promotionData, transaction) => {
+    let { promoCode, startDate, endDate, description, termsAndConditions, percentageDiscount, flatDiscount, usageLimit} = promotionData;
+    Checker.ifEmptyThrowError(id, Constants.Error.IdRequired);
+    let promotion = await Promotion.findByPk(id);
+    Checker.ifEmptyThrowError(promotion, Constants.Error.PromotionNotFound);
+    if(updateKeys.includes('promoCode')) {
+      Checker.ifEmptyThrowError(promoCode, 'Promo Code' + Constants.Error.XXXIsRequired);
+    }
+    if(updateKeys.includes('startDate')) {
+      Checker.ifEmptyThrowError(startDate, 'Start date' + Constants.Error.XXXIsRequired);
+      if(startDate > endDate) {
+        throw new CustomError(Constants.Error.StartDateLaterThanEndDate);
+      }
+    }
+    if(updateKeys.includes('endDate')) {
+      Checker.ifEmptyThrowError(endDate, 'End date' + Constants.Error.XXXIsRequired);
+      if(startDate > endDate) {
+        throw new CustomError(Constants.Error.StartDateLaterThanEndDate);
+      }
+    }
+    if(updateKeys.includes('usageLimit')) {
+    Checker.ifEmptyThrowError(usageLimit, 'Usage Limit' + Constants.Error.XXXIsRequired);
+    }
+    promotion = await Promotion.update(promotionData, { transaction, where: { id } });
   },
 
   retrieveMallPromotion: async() => {
-    
+    return await Promotion.findAll({ where: { promotionType: Constants.PromotionType.MallPromotion } });
   },
   
-  retrieveMerchantPromotionByMerchantId: async() => {
-    
+  retrieveMerchantPromotionByMerchantId: async(id) => {
+    Checker.ifEmptyThrowError(merchantId, 'Merchant ID' + Constants.Error.XXXIsRequired);
+    Checker.ifEmptyThrowError(await Merchant.findByPk(merchantId), Constants.Error.MerchantNotFound);
+    return await Promotion.findAll({ where: { merchantId: id, promotionType: Constants.PromotionType.MerchantPromotion } })
   },
 
-  deletePromotion: async(id) => {
+  deletePromotion: async(id, transaction) => {
     Checker.ifEmptyThrowError(id);
     let promotion = await Promotion.findByPk(id);
     Checker.ifEmptyThrowError(promotion, Constants.Error.ProductNotFound);
     if(promotion.used) {
       throw new CustomError(Constants.Error.PromotionCannotBeDeleted);
     }
-    await Promotion.destroy({ where: { id } });
+    await Promotion.update({ deleted: true }, { where: { id }, transaction });
   },
 }
