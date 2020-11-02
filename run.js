d8323cfe46581c9adefd20fc4a0fdc0760b93b44
Bryan Soh <bryansoh95@gmail.com>
2020-11-02 19:49:14 +0800

    [bryan_soh/ch170/setup-supertest-for-jest] Clean up.

--- run.js
+++ run.js
@@ -3,6 +3,7 @@ const app = require('./app');
 
 const host = config.get('server.host');
 const port = config.get('server.port');
+
 app.listen(port, host, () => {
   console.log('Server is running on port ', port);
 });
