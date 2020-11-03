9228c8e6ca2fa5d20fe90c4c8f20d6b8006c10ee
Bryan Soh <bryansoh95@gmail.com>
2020-11-03 19:03:21 +0800

    [bryan_soh/ch171/create-staff-test-suite] Added staff endpoint calls to testClient.

--- test/testClient.js
+++ test/testClient.js
@@ -1,10 +1,19 @@
 const supertest = require('supertest');
 const app = require('../app');
-//const request = supertest(app);
-// module.exports = {
-//   registerStaff: async (data) => request(app).post(/staff).send(data),
-//   retrieveStaff: async (id) => request(app).get(/staff/${id}),
-//   retrieveAllStaff: async () => request(app).get(/staff),
-//   updateStaff: async (id, updateDict) => request(app).put(/staff/${id}).send(updateDict),
-//   disableStaff: async (id) => request(app).put(/staff/${id}/disable)
-// };
+const request = supertest(app);
+
+module.exports = {
+  retrieveStaffRoles: async (token) => request.get(`/staff/staffRoles`).set('AuthToken', token),
+  retrieveStaff: async (token, id) => request.get(`/staff/${id}`).set('AuthToken', token),
+  retrieveAllStaff: async (token) => request.get(`/staff`).set('AuthToken', token),
+  updateStaffRole: async (token, id, data) => request.put(`/staff/staffRole/${id}`).send(data).set('AuthToken', token),
+  changePassword: async (token, id, data) => request.put(`/staff/${id}/changePassword`).send(data).set('AuthToken', token),
+  updateStaff: async (token, id, data) => request.put(`/staff/${id}`).send(data).set('AuthToken', token),
+  toggleDisableStaff: async (token, id) => request.put(`/staff/${id}/toggleDisable`).set('AuthToken', token),
+  loginStaff: async (data) => request.post(`/staff/login`).send(data),
+  retrieveStaffByEmail: async (token, data) => request.post(`/staff/email`).send(data).set('AuthToken', token),
+  sendResetPasswordEmail: async (data) => request.post(`/staff/forgotPassword`).send(data),
+  checkValidToken: async (data) => request.post(`/staff/resetPassword/checkValidToken`).send(data),
+  resetPassword: async (data) => request.post(`/staff/resetPassword`).send(data),
+  registerStaff: async (token, data) => request.post(`/staff`).send(data).set('AuthToken', token)
+};
