36691b74093bdcca00557921de21eb39f8048d66
Bryan Soh <bryansoh95@gmail.com>
2020-11-14 04:20:49 +0800

    [bryan_soh/ch176/cancel-order] Added function to cancel order.

--- app/common/constants.js
+++ app/common/constants.js
@@ -159,6 +159,7 @@ module.exports = {
     NoActiveBookingPackage: 'No active booking package available',
     NotificationNotFound: 'Notification Not Found',
     NoLockersAvailable: 'No lockers available',
+    OrderAlreadyCancelled: 'Order already cancelled',
     OrderNotFound: 'Order not found',
     OtpInvalid: 'OTP is invalid',
     PasswordCannotChange: 'Password cannot be changed',
